7325faafd80370ea3dc92174ce98383698ceb0d5
julia10ab <julia.arias.fg@gmail.com>
2025-02-24 15:04:33 +0100

    script inventario casi acabado

---
+++ Assets/Julia/Inventario/InventoryManager.cs
@@ -0,0 +1,87 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventoryManager : MonoBehaviour
+{
+    public static InventoryManager instance;
+    public List<InventoryObject> items = new List<InventoryObject>();
+
+    public Transform itemContent;
+    public GameObject inventoryItem;
+
+    public Toggle enableRemove;
+
+    public InventoryItemControler[] inventoryItems;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    public void Add(InventoryObject item)
+    {
+        items.Add(item);
+    }
+
+    public void Remove(InventoryObject item)
+    {
+        items.Remove(item);
+    }
+
+    public void ListItems()
+    {
+        foreach (Transform item in itemContent)
+        {
+            Destroy (item.gameObject);
+        }
+
+        foreach (var item in items)
+        {
+            GameObject obj = Instantiate(inventoryItem, itemContent);
+            var itemName = obj.transform.Find("ItemName").GetComponent<Text>();
+            var itemIcon = obj.transform.Find("ItemIcon").GetComponent<Image>();
+            var removeButton = obj.transform.Find("RemoveButton").GetComponent<Button>();
+
+            itemName.text = item.itemName;
+            itemIcon.sprite = item.icon;
+
+            if (enableRemove.isOn)
+            {
+                removeButton.gameObject.SetActive(true);
+            }
+        }
+
+        SetInventoryItems();
+    }
+
+    public void EnableItemsRemove()
+    {
+        if (enableRemove.isOn)
+        {
+            foreach (Transform item in itemContent)
+            {
+                item.Find("RemoveButton").gameObject.SetActive(true);
+            }
+        }
+
+        else
+        {
+            foreach (Transform item in itemContent)
+            {
+                item.Find("RemoveButton").gameObject.SetActive(true);
+            }
+        }
+    }
+
+    public void SetInventoryItems()
+    {
+        inventoryItems = itemContent.GetComponentsInChildren<InventoryItemControler>();
+
+        for (int i = 0; i < items.Count; i++)
+        {
+            inventoryItems[i].AddItem(items[i]);
+        }
+    }
+}
--- Assets/Julia/LevelManager.cs
+++ Assets/Julia/LevelManager.cs
@@ -9,21 +9,11 @@ public class LevelManager : MonoBehaviour
     public TextMeshProUGUI puertasText;
     public TextMeshProUGUI itemsText;
 
-    [Header("Inventario")]
-    public GameObject inventorySlotPrefab;
-    public Transform inventoryPanel;
-
-    //Lista de imágenes recogidas
-    public List<Sprite> collectedItems = new List<Sprite>();
-    public Sprite itemSprite;
-    
     private bool canInteract = false;
-    public GameObject currentItem; //Objeto con el que se interactua
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        //Sin textos al principio
         puertasText.text = "";
         itemsText.text = "";  
     }
@@ -33,18 +23,17 @@ public class LevelManager : MonoBehaviour
     {
         if (canInteract && Input.GetKeyDown(KeyCode.E))
         {
-            if (currentItem != null)
+            if (canInteract && Input.GetKeyDown (KeyCode.E))
             {
-                //Si el objeto es una puerta, solo interactuar
-                if (currentItem.CompareTag("Door"))
+                if (gameObject.tag == "Door")
                 {
-                    Debug.Log("Puerta Abierta");
+                    Debug.Log("Puerta abierta");
                 }
 
-                //Si es un ítem, añadir al inventario
-                else if (currentItem.CompareTag("Item"))
+                else if (gameObject.tag == "Item")
                 {
-                    CollectItem(currentItem);
+                    Debug.Log("Objeto recogido");
+                    Destroy(gameObject);
                 }
             }
         }
@@ -52,38 +41,30 @@ public class LevelManager : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (other.tag == "Player")
         {
-            if (gameObject.CompareTag("Door"))
+            if (gameObject.tag == "Door")
             {
                 puertasText.text = "(E) Abrir";
                 canInteract = true;
-                currentItem = other.gameObject;
             }
 
-            else if (gameObject.CompareTag("Item"))
+            else if (gameObject.tag == "Item")
             {
                 itemsText.text = "(E) Coger";
                 canInteract = true;
-                currentItem = other.gameObject;
             }
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (other.tag == "Player")
         {
             puertasText.text = "";
             itemsText.text = "";
             canInteract = false;
-            currentItem = null;
         }
-    }
-
-    private void CollectItem(GameObject item)
-    {
-
     }
 }
 
